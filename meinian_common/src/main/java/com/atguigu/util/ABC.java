d9bf5d59954958f95ef6e06806ac85ffa15ee80d
chenjiangda <chenjiangdada@163.com>
2022-06-18 00:24:52 +0800

    修改第五航代码，增加master

--- meinian_common/src/main/java/com/atguigu/util/ABC.java
+++ meinian_common/src/main/java/com/atguigu/util/ABC.java
@@ -2,6 +2,6 @@ package com.atguigu.util;
 
 public class ABC {
     public static void main(String[] args) {
-        System.out.println("11111111dev");
+        System.out.println("11111111devmaster");
     }
 }
