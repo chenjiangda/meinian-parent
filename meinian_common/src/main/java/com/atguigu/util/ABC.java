072550f7965830bdfb3d3d4e5453a78a909349a8
chenjiangda <chenjiangdada@163.com>
2022-06-18 13:37:56 +0800

    增加多行DEV

--- meinian_common/src/main/java/com/atguigu/util/ABC.java
+++ meinian_common/src/main/java/com/atguigu/util/ABC.java
@@ -4,5 +4,9 @@ public class ABC {
     public static void main(String[] args) {
         System.out.println("11111111devmaster");
         System.out.println("11111111devDEV");
+        System.out.println("11111111devDEV");
+        System.out.println("11111111devDEV");
+        System.out.println("11111111devDEV");
+        System.out.println("11111111devDEV");
     }
 }
